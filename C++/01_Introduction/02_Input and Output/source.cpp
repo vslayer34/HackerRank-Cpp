bd88baad04bca6306b1c2981ee36b3d97d7f33ea
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2023-10-17 17:52:00 +0300

    C++/Introduction/Input and Output
    https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true

---
+++ C++/01_Introduction/02_Input and Output/source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int num1 { };
+    int num2 { };
+    int num3 { };
+    std::cin >> num1;
+    std::cin >> num2;
+    std::cin >> num3;
+
+    std::cout << num1 + num2 + num3 << '\n';
+}
