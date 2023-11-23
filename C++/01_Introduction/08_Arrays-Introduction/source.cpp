5182e963c86f87affdc58984bbff7834684c9104
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2023-11-23 18:20:51 +0200

    Solved 01_Introduction/08_Arrays Introduction
    https://www.hackerrank.com/challenges/arrays-introduction/problem?isFullScreen=true

--- C++/01_Introduction/08_Arrays-Introduction/source.cpp
+++ C++/01_Introduction/08_Arrays-Introduction/source.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
 
 
+void displayArray(int array[], int arraySize)
+{
+    std::cout << "[ ";
+    for (int i = 0; i < arraySize; i++)
+    {
+        std::cout << array[i] << ((i != arraySize - 1) ? ", " : " ");
+    }
+    std::cout << "]";
+}
+
+void displayReservedArray(int array[], int arraySize)
+{
+    for (int i = arraySize - 1; i >= 0; i--)
+    {
+        std::cout << array[i] << " ";
+    }
+}
 
 int main()
 {
-    
+    // Print the integers of the array in the reverse order, space-separated on a single line.
+
+    // Sample Input
+    // 4
+    // 1 4 3 2   
+
+
+    // Sample Output
+    // 2 3 4 1
+
+    std::cout << "Please enter the array size: ";
+    int arraySize { };
+    std::cin >> arraySize;
+
+    int array[arraySize];
+
+    std::cout << "Please enter the array element seprated by space: ";
+
+    for (int i = 0; i < arraySize; i++)
+    {
+        std::cin >> array[i];
+    }
+
+    displayReservedArray(array, arraySize);
 }
